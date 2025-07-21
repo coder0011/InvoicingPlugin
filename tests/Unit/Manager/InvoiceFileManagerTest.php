b631ff08f9303b504dc2c88155f09e7694b2a8d2
Mateusz <mateusz@rustle.pl>
2025-07-21 10:34:06 +0200

    All PHPSpec rewrited to PHPUnit, some errors

---
+++ tests/Unit/Manager/InvoiceFileManagerTest.php
@@ -0,0 +1,57 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Sylius Sp. z o.o.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Tests\Sylius\InvoicingPlugin\Unit\Manager;
+
+use Gaufrette\FilesystemInterface;
+use PHPUnit\Framework\TestCase;
+use Sylius\InvoicingPlugin\Manager\InvoiceFileManager;
+use Sylius\InvoicingPlugin\Manager\InvoiceFileManagerInterface;
+use Sylius\InvoicingPlugin\Model\InvoicePdf;
+
+final class InvoiceFileManagerTest extends TestCase
+{
+    public function test_it_implements_invoice_file_saver_interface(): void
+    {
+        $filesystem = $this->createMock(FilesystemInterface::class);
+        $manager = new InvoiceFileManager($filesystem);
+
+        $this->assertInstanceOf(InvoiceFileManagerInterface::class, $manager);
+    }
+
+    public function test_it_saves_invoice_pdf_in_given_filesystem(): void
+    {
+        $filesystem = $this->createMock(FilesystemInterface::class);
+
+        $filesystem
+            ->expects($this->once())
+            ->method('write')
+            ->with('2020_01_01_invoice.pdf', 'CONTENT');
+
+        $manager = new InvoiceFileManager($filesystem);
+        $manager->save(new InvoicePdf('2020_01_01_invoice.pdf', 'CONTENT'));
+    }
+
+    public function test_it_removes_invoice_pdf_in_given_filesystem(): void
+    {
+        $filesystem = $this->createMock(FilesystemInterface::class);
+
+        $filesystem
+            ->expects($this->once())
+            ->method('delete')
+            ->with('2020_01_01_invoice.pdf');
+
+        $manager = new InvoiceFileManager($filesystem);
+        $manager->remove(new InvoicePdf('2020_01_01_invoice.pdf', 'CONTENT'));
+    }
+}
---
+++ tests/Unit/Model/InvoicePdfTest.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Sylius Sp. z o.o.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Tests\Sylius\InvoicingPlugin\Unit\Model;
+
+use PHPUnit\Framework\TestCase;
+use Sylius\InvoicingPlugin\Model\InvoicePdf;
+
+final class InvoicePdfTest extends TestCase
+{
+    public function test_it_has_filename(): void
+    {
+        $invoicePdf = new InvoicePdf('2018_01_0000002.pdf', 'pdf content');
+
+        $this->assertSame('2018_01_0000002.pdf', $invoicePdf->filename());
+    }
+
+    public function test_it_has_content(): void
+    {
+        $invoicePdf = new InvoicePdf('2018_01_0000002.pdf', 'pdf content');
+
+        $this->assertSame('pdf content', $invoicePdf->content());
+    }
+
+    public function test_it_has_full_path(): void
+    {
+        $invoicePdf = new InvoicePdf('2018_01_0000002.pdf', 'pdf content');
+        $invoicePdf->setFullPath('/full/path/invoice.pdf');
+
+        $this->assertSame('/full/path/invoice.pdf', $invoicePdf->fullPath());
+    }
+}
